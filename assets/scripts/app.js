f9690ae9a620f7356348613c2b6170d58d8fed1c
Bert <Bert>
2016-01-27 14:36:52 -0700

    floundering

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,4 @@
-function MakePlayer(name, pos, num){
+function MakePlayer(name, pos, num){ //also in the jQuery file ... may not need this version ...
     this.name = name;
     this.position = pos;
     this.numbber = num;
--- assets/scripts/jq-magic.js
+++ assets/scripts/jq-magic.js
@@ -1,14 +1,22 @@
 var team = [];
 function addTile(){
+    //debugger;
     var pName = document.getElementById("player-name").value;
         var pPosition = document.getElementById("player-position").value;
             var pNumber = document.getElementById("player-number").value;
-            nextDude = new Player( pName, pPosition, pNumber);
+            var nextDude = new Player( pName, pPosition, pNumber);
+            myPlayer(nextDude);
 }
- <img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="">
+ //<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="">
  
  function Player(pName, pPosition, pNumber){
-     this.name =pName;
-     this.position=pPosition;
+     this.name = pName;
+     this.position = pPosition;
      this.numbber = pNumber;
+     this.removeBtn = "<button>Remove</button>"
+ }
+ 
+ // adds card with "draft" button
+ function myPlayer(nextDude){
+     $("player-card").append(nextDude);
  }
