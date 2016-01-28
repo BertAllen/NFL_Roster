b9c655843c5979d2506fcd67d068d8fbc6869ee8
Bert <Bert>
2016-01-28 14:34:45 -0700

    everything working!

--- assets/scripts/jq-magic.js
+++ assets/scripts/jq-magic.js
@@ -1,5 +1,20 @@
 var team = [];
+var pid = 0;
+//adds players to array
+function draftGuy(nextDude){
+    team.push(nextDude);
+}
 
+//removes player from array
+function cutGuy(id){
+    id = Number(id);
+    for(var i=0; i<team.length; i++){
+        if(team[i].id === id){
+            team.splice(i, 1);
+            return;
+        }
+    }
+}
 
 function addTile(e) {
     e.preventDefault()
@@ -9,23 +24,26 @@ function addTile(e) {
     var pPosition = document.getElementById("player-position").value;
     var pNumber = document.getElementById("player-number").value;
     var nextDude = new Player(pName, pPosition, pNumber);
+    draftGuy(nextDude);
     myPlayer(nextDude);
 }
 //<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="">
  
-function Player(pName, pPosition, pNumber) {
+function Player(pName, pPosition, pNumber) { //player constructor function
     //debugger;
+    this.id = pid;
     this.name = pName;
     this.position = pPosition;
     this.numbber = pNumber;
-    this.removeBtn = '<button class="cut">Remove</button>'
+    this.removeBtn = '<button class="cut" id="'+this.id+'">Remove</button>';
+    pid++;
 }
  
 // adds card with "draft" button
 function myPlayer(nextDude) {
     var wholeTile = '<div class="player-card">'
     var facePlate = '<img src="http://s.nflcdn.com/static/content/public/image/fantasy/transparent/200x200/" alt="">';
-    var namePlate = '<h1>' + nextDude.name + '</h1>';
+    var namePlate = '<h1 id="name">' + nextDude.name + '</h1>';
     var posPlate = '<h4>' + nextDude.position + '</h4>';
     var numPlate = '<h2># ' + nextDude.numbber + '</h2>';
     wholeTile = wholeTile + facePlate + namePlate + posPlate + numPlate + nextDude.removeBtn + "</div>"
@@ -45,6 +63,10 @@ function myPlayer(nextDude) {
 //}
 //$(document).ready(function(){
     $('.player-roster').on("click", 'button', function(){
+        
+        var pId = this.id;
+        //debugger;
+        cutGuy(pId);
         $(this).parent().remove();
     });
 //});
